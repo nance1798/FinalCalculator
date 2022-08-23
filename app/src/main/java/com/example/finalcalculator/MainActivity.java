ebf95a4a84db7887ee631d855812258494cf17e2
nance1798 <nance1798@students.d211.org>
2022-08-23 09:30:22 -0500

    Got the code to work and I have a calculator now

--- app/src/main/java/com/example/finalcalculator/MainActivity.java
+++ app/src/main/java/com/example/finalcalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,33 +16,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void switchScreens(View view){
-        // Create a reference within the java file for this screen to the
-        //xml file for the design elements displayed on the screen
-        // This will only work if you have an element of this type and
-        // this id within the xml file that you referenced in the onCreate
+    public void findSum(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
 
-        EditText editText = findViewById(R.id.enteredName);
-        String name = editText.getText().toString();
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double sum = num1 + num2;
 
-        /*
-        To send the app to another screen, you need to create an Intent.
-        This is equivalent to getting out a new envelope to mail a letter.
-        In the constructor, the first param is telling you where you are coming
-        from, and we usually use this because we are coming from this screen
-
-        The second param tells you which activity you are going to, and you need
-        to use the ActivityName.class
-
-        After you create the intent, if you want to send data with it, you need
-        to use putExtra and the first param is a label, usually all caps, and
-        the second param is the value you are sending.
-
-        Lastly, you call startActivity to launch the new intent and switch screens.
-         */
-
-        Intent intent = new Intent(this,SecondActivity.class);
-        intent.putExtra("NAME",name);
-        startActivity(intent);
+        numberSumTV.setText("" + sum);
     }
+
+    public void findDifference(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberDifferenceTV = findViewById(R.id.resultTV);
+
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double difference = num1 - num2;
+
+        numberDifferenceTV.setText("" + difference);
+    }
+
+    public void findProduct(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberProductTV = findViewById(R.id.resultTV);
+
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double product = num1 * num2;
+
+        numberProductTV.setText("" + product);
+    }
+
+    public void findQuotient(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberQuotientTV = findViewById(R.id.resultTV);
+
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double quotient = num1 / num2;
+
+        numberQuotientTV.setText("" + quotient);
+    }
+
 }
+
--- app/src/main/java/com/example/finalcalculator/SecondActivity.java
+++ app/src/main/java/com/example/finalcalculator/SecondActivity.java
@@ -13,13 +13,6 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent intent = getIntent();
-        // retrieving the data that was sent and labeled as NAME
-        // there are many methods for getting different data types
-        String nameToShow = intent.getStringExtra("Name");
-        // getting a reference to the textView in the xml file for THIS activity
-        TextView textView = findViewById(R.id.userName);
-        // change the text for the textView on this screen
-        textView.setText(nameToShow);
+
     }
 }
