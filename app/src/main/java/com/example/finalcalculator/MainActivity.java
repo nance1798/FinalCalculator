3bf04cfacd6e42c1f8d06f3493c921064c85a4ba
nance1798 <nance1798@students.d211.org>
2022-08-25 07:57:28 -0500

    I finished by adding the try catch enhancement

--- app/src/main/java/com/example/finalcalculator/MainActivity.java
+++ app/src/main/java/com/example/finalcalculator/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView comment = findViewById(R.id.commentCode);
+        comment.setText("");
+
         double num1;
         double num2;
         double sum = 0;
@@ -28,9 +31,7 @@ public class MainActivity extends AppCompatActivity {
             num2 = Integer.parseInt(number2ET.getText().toString());
             sum = num1 + num2;
         } catch (Exception e) {
-            // Tomorrow I will add an area here that gets the text of a new text box
-            // that prints a message saying to retry and enter an integer
-
+            comment.setText("Please enter a valid integer and try again.");
         }
 
         numberSumTV.setText("" + sum);
@@ -40,10 +41,19 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberDifferenceTV = findViewById(R.id.resultTV);
+        TextView comment = findViewById(R.id.commentCode);
+        comment.setText("");
 
-        double num1 = Integer.parseInt(number1ET.getText().toString());
-        double num2 = Integer.parseInt(number2ET.getText().toString());
-        double difference = num1 - num2;
+        double num1;
+        double num2;
+        double difference = 0;
+        try {
+            num1 = Integer.parseInt(number1ET.getText().toString());
+            num2 = Integer.parseInt(number2ET.getText().toString());
+            difference = num1 - num2;
+        } catch (Exception e){
+            comment.setText("Please enter a valid integer and try again.");
+        }
 
         numberDifferenceTV.setText("" + difference);
     }
@@ -52,10 +62,19 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberProductTV = findViewById(R.id.resultTV);
+        TextView comment = findViewById(R.id.commentCode);
+        comment.setText("");
 
-        double num1 = Integer.parseInt(number1ET.getText().toString());
-        double num2 = Integer.parseInt(number2ET.getText().toString());
-        double product = num1 * num2;
+        double num1;
+        double num2;
+        double product = 0;
+        try{
+            num1 = Integer.parseInt(number1ET.getText().toString());
+            num2 = Integer.parseInt(number2ET.getText().toString());
+            product = num1 * num2;
+        } catch (Exception e){
+            comment.setText("Please enter a valid integer and try again.");
+        }
 
         numberProductTV.setText("" + product);
     }
@@ -64,10 +83,19 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberQuotientTV = findViewById(R.id.resultTV);
+        TextView comment = findViewById(R.id.commentCode);
+        comment.setText("");
 
-        double num1 = Integer.parseInt(number1ET.getText().toString());
-        double num2 = Integer.parseInt(number2ET.getText().toString());
-        double quotient = num1 / num2;
+        double num1;
+        double num2;
+        double quotient = 0;
+        try {
+            num1 = Integer.parseInt(number1ET.getText().toString());
+            num2 = Integer.parseInt(number2ET.getText().toString());
+            quotient = num1 / num2;
+        } catch (Exception e){
+            comment.setText("Please enter a valid integer and try again.");
+        }
 
         numberQuotientTV.setText("" + quotient);
     }
